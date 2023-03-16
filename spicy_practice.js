a14b9d1e21c8faac72d5c93a7b5e33eea13214ff
ileini-melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 13:31:06 -0400

    Update spicy_practice.js

--- spicy_practice.js
+++ spicy_practice.js
@@ -1,3 +1,14 @@
 // Intro to Code with p5.js: Variables and Random
 // Use this file to complete the "Spicy Practice": Follow a lot of our procedures from this lesson, but instead of focusing on randomness, try making an emoji stamp that appears each time you click..
 
+
+function setup(){
+  createCanvas(800, 600);
+  
+}
+
+function draw(){
+  background(220);
+
+  
+}
