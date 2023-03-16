ad860c6ee513dd23ec819ba4ff2e294be468dcd4
Ileini Melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 17:05:23 +0000

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 600);
+  background(220);
+}
+
+function draw() {
+  // Background
+  background(220);
+  // Additional draw() code
+  fill(0);
+  text("Hello!", 20, 20);
+  ellipse(250, 300, 10);
+}
+
+function mousePressed() {
+  console.log("You clicked the mouse!");
+}
